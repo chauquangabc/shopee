cf12f7ef625b0ded7173a6a428cbc066956a9dda
chauquangabc <chauquangabc@gmail.com>
2025-04-12 19:02:16 +0700

    Cập nhật UI sản phẩm

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Product {
   final String title, description;
   final String id;
   final List<String> images;
-  final int price, saleQuantity;
-  final double rating, discount;
+  final int saleQuantity;
+  final double price,  rating, discount;
   final List<dynamic> feedback;
 
   Product({
@@ -17,84 +19,73 @@ class Product {
     this.saleQuantity = 0,
     this.feedback =  const [],
   });
-
-  factory Product.fromFirestore(Map<String, dynamic> json) {
+  factory Product.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
     return Product(
-      id: json['id'] ?? '',
-      title: json['title'] ?? '',
-      description: json['description'] ?? '',
-      images: List<String>.from(json['images'] ?? []),
-      price: json['price'] ?? 0,
-      discount: (json['discount'] ?? 0).toDouble(),
-      rating: (json['rating'] ?? 0).toDouble(),
-      saleQuantity: json['saleQuantity'] ?? 0,
-      feedback: List<dynamic>.from(json['feedback'] ?? []),
+      id: doc.id,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      images: List<String>.from(data['images'] ?? []),
+      price: (data['price'] ?? 0).toDouble(),
+      discount: (data['discount'] ?? 0).toDouble(),
+      saleQuantity: data['saleQuantity'] ?? 0,
     );
   }
 }
 
-final List<Product> demoProducts = [
-  Product(
-    id: '1',
-    title: 'Áo dsquare2',
-    description: 'Đẹp vl ',
-    images: ['assets/dsq2.webp', 'assets/dsq2_1.webp', 'assets/dsq2_3.webp'],
-    price: 150000,
-    discount: 15,
-    saleQuantity: 5,
-  ),
-  Product(
-    id: '2',
-    title: 'Bộ quần áo mùa hè siêu thoáng mát siêu đẹp siêu rẻ siêu xịn ! ',
-    description: 'Giá rẻ bất ngờ',
-    images: ['assets/img_clothes1.webp'],
-    saleQuantity: 50,
-    discount: 20,
-    price: 200000,
-  ),
-  Product(
-    id: '3',
-    title: 'Bộ quần áo mùa hè ',
-    description: 'Giá rẻ bất ngờ',
-    images: ['assets/img_clothes2.jpg'],
-    saleQuantity: 40,
-    discount: 30,
-    price: 500000,
-  ),
-  Product(
-    id: '4',
-    title: 'Bộ quần áo mùa hè 2',
-    description: 'Giá rẻ bất ngờ',
-    images: ['assets/img_clothes3.jpg'],
-    saleQuantity: 60,
-    discount: 50,
-    price: 60000,
-  ),
-  Product(
-    id: '5',
-    title: 'Bộ quần áo mùa hè 3',
-    description: 'Giá rẻ bất ngờ',
-    images: ['assets/img_clothes4.jpg'],
-    saleQuantity: 10,
-    price: 80000,
-    discount: 50,
-  ),
-  Product(
-    id: '6',
-    title: 'Bộ quần áo mùa hè 4',
-    description: 'Giá rẻ bất ngờ',
-    images: ['assets/img_clothes5.jpg'],
-    discount: 25,
-    saleQuantity: 35,
-    price: 90000,
-  ),
-  Product(
-    id: '7',
-    title: 'Bộ quần áo mùa hè 5',
-    description: 'Giá rẻ bất ngờ',
-    images: ['assets/img_clothes4.jpg'],
-    saleQuantity: 15,
-    price: 90000,
-    discount: 50,
-  ),
-];
+// final List<Product> demoProducts = [
+//   Product(
+//     id: '1',
+//     title: 'Áo dsquare2',
+//     description: 'Đẹp vl ',
+//     images: ['assets/dsq2.webp', 'assets/dsq2_1.webp', 'assets/dsq2_3.webp'],
+//     price: 150000,
+//     discount: 15,
+//     saleQuantity: 5,
+//   ),
+//   Product(
+//     id: '2',
+//     title: 'Bộ quần áo mùa hè siêu thoáng mát siêu đẹp siêu rẻ siêu xịn ! ',
+//     description: 'Giá rẻ bất ngờ',
+//     images: ['assets/img_clothes1.webp'],
+//     saleQuantity: 50,
+//     discount: 20,
+//     price: 200000,
+//   ),
+//   Product(
+//     id: '3',
+//     title: 'Bộ quần áo mùa hè ',
+//     description: 'Giá rẻ bất ngờ',
+//     images: ['assets/img_clothes2.jpg'],
+//     saleQuantity: 40,
+//     discount: 30,
+//     price: 500000,
+//   ),
+//   Product(
+//     id: '4',
+//     title: 'Bộ quần áo mùa hè 2',
+//     description: 'Giá rẻ bất ngờ',
+//     images: ['assets/img_clothes3.jpg'],
+//     saleQuantity: 60,
+//     discount: 50,
+//     price: 60000,
+//   ),
+//   Product(
+//     id: '5',
+//     title: 'Bộ quần áo mùa hè 3',
+//     description: 'Giá rẻ bất ngờ',
+//     images: ['assets/img_clothes4.jpg'],
+//     saleQuantity: 10,
+//     price: 80000,
+//     discount: 50,
+//   ),
+//   Product(
+//     id: '6',
+//     title: 'Bộ quần áo mùa hè 4',
+//     description: 'Giá rẻ bất ngờ',
+//     images: ['assets/img_clothes5.jpg'],
+//     discount: 25,
+//     saleQuantity: 35,
+//     price: 90000,
+//   ),
+// ];
--- lib/page_unloggin/home_page/body_home_page/body_component3.dart
+++ lib/page_unloggin/home_page/body_home_page/body_component3.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
 import 'package:shopee/models/products.dart';
 
@@ -14,89 +16,109 @@ class Component3 extends StatefulWidget {
 
 class _Component3State extends State<Component3> {
   final formatCurrency = NumberFormat('#,##0', 'vi_VN');
-
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisExtent: 250,
-      ),
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        final product = demoProducts[index];
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => DetailsProduct(products: product)
-                )
-            );
-          },
-          child: Container(
-            height: 200,
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 5),
-                  height: 150,
-                  width: double.infinity,
-                  child: Image.asset(product.images[0], fit: BoxFit.contain),
+    return FutureBuilder(
+      future: FirebaseFirestore.instance.collection('products').get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (!snapshot.hasData) {
+          return Text("No data here");
+        }
+        final products =
+            snapshot.data!.docs
+                .map((doc) => Product.fromFirestore(doc))
+                .toList();
+
+        return GridView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisExtent: 250,
+          ),
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final product = products[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsProduct(products: product),
+                  ),
+                );
+              },
+              child: Container(
+                height: 200,
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                SizedBox(height: 5),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: SizedBox(
-                    height: 40,
-                    child: Center(
-                      child: Text(
-                        product.title,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        textHeightBehavior: TextHeightBehavior(
-                          applyHeightToFirstAscent: false,
-                          applyHeightToLastDescent: false,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 5),
+                      height: 150,
+                      width: double.infinity,
+                      child:
+                      product.images.isNotEmpty
+                          ? Image.network(
+                            product.images[0],
+                            fit: BoxFit.contain,
+                          )
+                          : Icon(Icons.image_not_supported),
+                    ),
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: SizedBox(
+                        height: 40,
+                        child: Center(
+                          child: Text(
+                            product.title,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textHeightBehavior: TextHeightBehavior(
+                              applyHeightToFirstAscent: false,
+                              applyHeightToLastDescent: false,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
+                    SizedBox(height: 5),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Text(
-                          'đ',
-                          style: TextStyle(
-                            color: Colors.red,
-                            fontSize: 12,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.red,
-                          ),
+                        Row(
+                          children: [
+                            Text(
+                              'đ',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 12,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.red,
+                              ),
+                            ),
+                            SizedBox(width: 1),
+                            Text(
+                              '${formatCurrency.format(product.price * (100 - product.discount) / 100)}',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 1),
-                        Text(
-                          '${formatCurrency.format(product.price * (100 - product.discount) /100)}',
-                          style: TextStyle(color: Colors.red),
-                        ),
+                        Text(' Đã bán ${product.saleQuantity}'),
                       ],
                     ),
-                    Text(' Đã bán ${product.saleQuantity}'),
                   ],
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         );
       },
     );
